82ab3e5592c53c2a8b2921a9a07351b5447184dc
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-03-24 15:47:32 +0900

    dependecies

---
+++ src/test/java/websocketchat/websocketchat/WebSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package websocketchat.websocketchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
