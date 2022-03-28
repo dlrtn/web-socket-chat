3db95a7f025210d23a5c6d1fda8133272aaf6f4e
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-03-28 23:18:09 +0900

    중간 커밋

--- src/main/java/com/dlrtn/websocket/chat/mapper/UserMapper.java
+++ src/main/java/com/dlrtn/websocket/chat/mapper/UserMapper.java
@@ -1,9 +1,9 @@
 package com.dlrtn.websocket.chat.mapper;
 
-import com.dlrtn.websocket.chat.vo.UserVo;
+import com.dlrtn.websocket.chat.vo.User;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface UserMapper {
-    void saveUser(UserVo userVo);
+    void saveUser(User user);
 }
