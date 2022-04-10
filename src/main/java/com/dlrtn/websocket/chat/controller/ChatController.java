994a52c68175ec9c7e195236db2d86b49a41a522
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-04-10 21:01:13 +0900

    레퍼런스 https://dev-gorany.tistory.com/212?category=901854
    websocket chating 로직 추가, front 단에서 오류 나는 것 확인
    
    11일에 확인 바람

---
+++ src/main/java/com/dlrtn/websocket/chat/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.dlrtn.websocket.chat.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    @Operation(summary = "채팅 내용 수신!!")
+    @GetMapping("/chat")
+    public String chatGET(){
+        return "chat";
+    }
+}
