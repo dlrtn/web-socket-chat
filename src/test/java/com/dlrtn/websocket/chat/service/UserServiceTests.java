509dc1f155c5f5d1f5220fe6b572a2c270ce09e8
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-04-28 11:13:12 +0900

    Review 내용 관련 수정

--- src/test/java/com/dlrtn/websocket/chat/service/UserServiceTests.java
+++ src/test/java/com/dlrtn/websocket/chat/service/UserServiceTests.java
@@ -1,19 +1,26 @@
 package com.dlrtn.websocket.chat.service;
 
 import com.dlrtn.websocket.chat.config.LightMyBatisTest;
+import com.dlrtn.websocket.chat.model.ResponseMessage;
 import com.dlrtn.websocket.chat.model.UserAuthRole;
+import com.dlrtn.websocket.chat.model.UserSessionConstants;
 import com.dlrtn.websocket.chat.model.payload.CommonResponse;
 import com.dlrtn.websocket.chat.model.payload.SignInRequest;
 import com.dlrtn.websocket.chat.model.payload.SignUpRequest;
+import com.dlrtn.websocket.chat.util.CookieUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@LightMyBatisTest
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpRequest;
+
+@SpringBootTest
 public class UserServiceTests {
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private UserService userService;
 
@@ -23,38 +30,15 @@ public class UserServiceTests {
 
         SignUpRequest request = new SignUpRequest();
 
-        request.setUserId("dlrtn");
-        request.setPassword("1234");
+        request.setUserId("11");
+        request.setPassword("22");
         request.setRealName("wndlrtn");
         request.setAuthRole(UserAuthRole.USER);
 
         CommonResponse commonResponse = userService.signUp(request);
 
-        Assertions.assertAll(
-                () -> Assertions.assertEquals("Success", commonResponse)
-        );
+        Assertions.assertEquals(ResponseMessage.SUCCESS, commonResponse.getMessage());
 
     }
 
-    @DisplayName("유저 로그인 테스트")
-    @Test
-    void login_user_test() {
-        SignInRequest request = new SignInRequest();
-
-        request.setUserId("dlrtn");
-        request.setPassword("1234");
-
-        CommonResponse commonResponse = userService.signIn(request);
-
-        Assertions.assertAll(
-                () -> Assertions.assertEquals("Success", commonResponse)
-        );
-    }
-
-    @DisplayName("유저 회원탈퇴 테스트")
-    @Test
-    void delete_user_test() {
-
-
-    }
 }
