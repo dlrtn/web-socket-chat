fd2cb313abecab62ce7e5c80a176cf01802e1d97
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-03 15:03:48 +0900

    회원 찾기, 정보 변경 테스트 코드 작성

--- src/test/java/com/dlrtn/websocket/chat/service/UserServiceTests.java
+++ src/test/java/com/dlrtn/websocket/chat/service/UserServiceTests.java
@@ -6,13 +6,11 @@ import com.dlrtn.websocket.chat.model.UserAuthRole;
 import com.dlrtn.websocket.chat.model.UserSessionConstants;
 import com.dlrtn.websocket.chat.model.UserSessionCreation;
 import com.dlrtn.websocket.chat.model.domain.User;
-import com.dlrtn.websocket.chat.model.payload.CommonResponse;
-import com.dlrtn.websocket.chat.model.payload.DeleteUserRequest;
-import com.dlrtn.websocket.chat.model.payload.SignInRequest;
-import com.dlrtn.websocket.chat.model.payload.SignUpRequest;
+import com.dlrtn.websocket.chat.model.payload.*;
 
 import com.dlrtn.websocket.chat.repository.InMemorySessionRepository;
 import com.dlrtn.websocket.chat.util.CookieUtils;
+import com.fasterxml.jackson.databind.jsontype.impl.AsExistingPropertyTypeSerializer;
 import org.junit.jupiter.api.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +19,11 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
 
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 @SpringBootTest
 public class UserServiceTests {
-
-    @Autowired
-    private UserMapper userMapper;
 
     private MockHttpServletRequest request;
     private MockHttpServletResponse response;
@@ -79,6 +75,33 @@ public class UserServiceTests {
 
     }
 
+    @DisplayName("유저 회원가입 기능 부분 테스트")
+    @Test
+    void join_user_test_existed_user() {
+
+        SignUpRequest request = new SignUpRequest();
+
+        request.setUserId("66");
+        request.setPassword("77");
+        request.setRealName("wndlrtn");
+        request.setAuthRole(UserAuthRole.USER);
+
+        CommonResponse commonResponse = userService.signUp(request);
+
+        Assertions.assertEquals(ResponseMessage.EXISTED_USER_ID, commonResponse.getMessage());
+
+    }
+
+    @DisplayName("유저 찾기 기능 부분 테스트")
+    @Test
+    void find_user_test() {
+
+        String userId = "11";
+
+        User foundUser = userService.findOne(userId);
+
+    }
+
     @DisplayName("유저 로그인 기능 부분 테스트")
     @Test
     void login_user_test() {
@@ -94,6 +117,28 @@ public class UserServiceTests {
 
     }
 
+    @DisplayName("유저 회원정보 변경 기능 부분 테스트")
+    @Test
+    void update_user_test() {
+
+        UserInfoUpdateRequest requestBody = new UserInfoUpdateRequest();
+
+        requestBody.setUserId("11");
+        requestBody.setExistingPassword("22");
+        requestBody.setNewRealName("1234");
+        requestBody.setNewPassword("123");
+
+        CommonResponse commonResponse = userService.update(sessionId, requestBody);
+
+        User foundUser = userService.findOne(requestBody.getUserId());
+
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(requestBody.getUserId(), foundUser.getUserId()),
+                () -> Assertions.assertEquals(requestBody.getNewPassword(), foundUser.getPassword()),
+                () -> Assertions.assertEquals(requestBody.getNewRealName(), foundUser.getRealName()));
+
+    }
+
     @DisplayName("유저 회원탈퇴 기능 부분 테스트")
     @Test
     void withdrawal_user_test() {
@@ -103,7 +148,7 @@ public class UserServiceTests {
         requestBody.setUserId("66");
         requestBody.setPassword("77");
 
-        User foundUser = userMapper.findByUserId(requestBody.getUserId());
+        User foundUser = userService.findOne(requestBody.getUserId());
 
         CommonResponse commonResponse = userService.deleteUser(sessionId, requestBody);
 
