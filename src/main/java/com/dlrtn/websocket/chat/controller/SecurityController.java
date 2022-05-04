8e6c480270dea4a5b38af5f1f1eb6ebce4e2912c
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-04 16:48:23 +0900

    SecurityController 기본 설정 및 리다이렉트 정의

--- src/main/java/com/dlrtn/websocket/chat/controller/SecurityController.java
+++ src/main/java/com/dlrtn/websocket/chat/controller/SecurityController.java
@@ -1,5 +1,33 @@
 package com.dlrtn.websocket.chat.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RequiredArgsConstructor
 public class SecurityController {
+
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping(value = "/sign-in")
+    public String customLoginProcess(
+            @RequestParam String username,
+            @RequestParam String password
+    ) {
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+
+        Authentication authentication = authenticationManager.authenticate(token);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return "redirect:/sign-up";
+
+    }
+
 
 }
