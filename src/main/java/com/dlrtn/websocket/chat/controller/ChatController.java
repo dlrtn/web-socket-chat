f20d0cf33b304116b75a5a700a080f4acf0672e0
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-16 23:18:13 +0900

    ChatController 코드 수정

--- src/main/java/com/dlrtn/websocket/chat/controller/ChatController.java
+++ src/main/java/com/dlrtn/websocket/chat/controller/ChatController.java
@@ -1,7 +1,11 @@
 package com.dlrtn.websocket.chat.controller;
 
+import com.dlrtn.websocket.chat.model.domain.ChatMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +14,24 @@ import static com.dlrtn.websocket.chat.model.PagePathConstants.*;
 
 @Controller
 @RequestMapping(CHAT)
-@RequiredArgsConstructor
 public class ChatController {
 
-    @Operation(summary = "채팅 내용 수신!!")
-    @GetMapping(MAIN)
-    public String chatGET() {
-        return MAIN;
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    public ChatController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
+    @MessageMapping("/join")
+    public void join(ChatMessage message) {
+        message.setMessage(message.getWriter() + "님이 입장하셨습니다.");
+        template.convertAndSend("/subscribe/chat/room/" + message.getChatRoomId(), message);
+    }
+
+    @MessageMapping("/message")
+    public void message(ChatMessage message) {
+        template.convertAndSend("/subscribe/chat/room/" + message.getChatRoomId(), message);
     }
 
 }
