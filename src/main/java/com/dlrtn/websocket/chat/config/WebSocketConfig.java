c8aece023a7694c72758b1a1c7373244dc31cd65
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-19 14:57:37 +0900

    STOMP에서 WebSocket로 변경 후, 연결 테스트

--- src/main/java/com/dlrtn/websocket/chat/config/WebSocketConfig.java
+++ src/main/java/com/dlrtn/websocket/chat/config/WebSocketConfig.java
@@ -1,22 +1,25 @@
 package com.dlrtn.websocket.chat.config;
 
+import lombok.RequiredArgsConstructor;
+import com.dlrtn.websocket.chat.handler.ChatHandler;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatHandler chatHandler;
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOrigins("*").withSockJS();
-    }
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/publish");
-        registry.enableSimpleBroker("/subscribe");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatHandler, "/ws/chat")
+                .setAllowedOrigins("*")
+                ;
     }
 
 }
