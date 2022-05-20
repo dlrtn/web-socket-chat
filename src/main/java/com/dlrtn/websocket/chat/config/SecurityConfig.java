98255b0da9904f6540239ca19120ebf3c53c77de
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-20 18:38:50 +0900

    /stomp/chat url에 대한 접근 허용

--- src/main/java/com/dlrtn/websocket/chat/config/SecurityConfig.java
+++ src/main/java/com/dlrtn/websocket/chat/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/", LOGIN, SIGNUP, "ws/chat").permitAll()
+                .mvcMatchers("/", LOGIN, SIGNUP, "stomp/chat").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().permitAll();
 
