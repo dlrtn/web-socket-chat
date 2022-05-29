e260f46794004a99ddc48bfb8a39a108baf7aa87
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-05-30 00:44:19 +0900

    chatRoomRepo 기본 CRUD 로직 생성

--- src/main/java/com/dlrtn/websocket/chat/business/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/dlrtn/websocket/chat/business/chat/repository/ChatRoomRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.jooq.DSLContext;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import static com.dlrtn.websocket.chat.business.chat.model.domain.generated.study_db.tables.TbChatroom.TB_CHATROOM;
 
 @Repository
@@ -14,7 +16,9 @@ public class ChatRoomRepository {
     private final DSLContext dslContext;
 
     public boolean makeRoom(ChatRoom chatRoom) {
-        return false;
+        return dslContext.insertInto(TB_CHATROOM, TB_CHATROOM.ROOMID, TB_CHATROOM.NAME)
+                .values(chatRoom.getRoomId(), chatRoom.getName())
+                .execute() == 1;
     }
 
     public ChatRoom findByRoomId(String roomId) {
@@ -24,10 +28,26 @@ public class ChatRoomRepository {
                 .fetchOneInto(ChatRoom.class);
     }
 
+    public List<ChatRoom> findAll() {
+        return dslContext.select()
+                .from(TB_CHATROOM)
+                .fetchInto(ChatRoom.class);
+    }
+
+    public boolean updateRoomInfo(String roomId, String name) {
+        return dslContext.update(TB_CHATROOM)
+                .set(TB_CHATROOM.NAME, name)
+                .where(TB_CHATROOM.ROOMID.eq(roomId))
+                .execute() == 1; //TODO 과연 좋은 방법인가..
+
+    }
+
     public boolean delete(String roomId) {
         return dslContext.delete(TB_CHATROOM)
                 .where(TB_CHATROOM.ROOMID.equal(roomId))
                 .execute() == 1;
     }
+
+
 
 }
