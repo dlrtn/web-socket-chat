e08407691bc96533cf8e50cdffcf9bda8afebd52
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-06-05 20:23:52 +0900

    FriendService 로직 수정 및 Payload 객체 수정에 따른 UserService 수정

--- src/main/java/com/dlrtn/websocket/chat/business/user/application/FriendService.java
+++ src/main/java/com/dlrtn/websocket/chat/business/user/application/FriendService.java
@@ -1,11 +1,16 @@
 package com.dlrtn.websocket.chat.business.user.application;
 
+import com.dlrtn.websocket.chat.business.user.exception.FriendNotExistsException;
 import com.dlrtn.websocket.chat.business.user.model.domain.User;
+import com.dlrtn.websocket.chat.business.user.model.payload.AddFriendResponse;
+import com.dlrtn.websocket.chat.business.user.model.payload.ChangeFriendStateRequest;
+import com.dlrtn.websocket.chat.business.user.model.payload.ChangeFriendStateResponse;
+import com.dlrtn.websocket.chat.business.user.model.payload.DeleteFriendResponse;
 import com.dlrtn.websocket.chat.business.user.repository.FriendRepository;
 import com.dlrtn.websocket.chat.business.user.repository.InMemorySessionRepository;
-import com.dlrtn.websocket.chat.common.model.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
@@ -14,54 +19,56 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class FriendService {
 
-    FriendRepository friendRepository;
-    InMemorySessionRepository sessionRepository;
+    private final FriendRepository friendRepository;
+    private final InMemorySessionRepository sessionRepository;
 
-    public CommonResponse addFriend(String sessionId, String friendId) {
+    @Transactional
+    public AddFriendResponse addFriend(String sessionId, String friendId) {
         User sessionUser = sessionRepository.get(sessionId);
         if (Objects.nonNull(friendRepository.selectFriend(sessionUser, friendId))) {
-            return CommonResponse.failWith("Not Exists in friend list");
+            throw new FriendNotExistsException();
         }
 
         friendRepository.insertIntoFriendList(sessionUser, friendId);
-        return CommonResponse.success();
+        return AddFriendResponse.success();
     }
 
-    public CommonResponse deleteFriend(String sessionId, String friendId) {
+    @Transactional
+    public DeleteFriendResponse deleteFriend(String sessionId, String friendId) {
         User sessionUser = sessionRepository.get(sessionId);
         if (Objects.nonNull(friendRepository.selectFriend(sessionUser, friendId))) {
-            return CommonResponse.failWith("Not Exists in friend list");
+            throw new FriendNotExistsException();
         }
 
         friendRepository.deleteUserFromFriendList(sessionUser, friendId);
-        return CommonResponse.success();
+        return DeleteFriendResponse.success();
     }
 
     public List<User> getFriends(String sessionId) {
         User sessionUser = sessionRepository.get(sessionId);
-
-        return friendRepository.selectAllFriends(sessionUser);
-    }
-
-    public CommonResponse addFavoriteFriendList(String sessionId, String friendId) {
-        User sessionUser = sessionRepository.get(sessionId);
-        if (Objects.nonNull(friendRepository.selectFriend(sessionUser, friendId))) {
-            return CommonResponse.failWith("Not Exists in friend list");
+        List<User> foundFriends = friendRepository.selectAllFriends(sessionUser);
+        if (Objects.isNull(foundFriends)) {
+            throw new FriendNotExistsException();
         }
 
-        friendRepository.updateFriendFavorite(sessionUser, friendId);
-        return CommonResponse.success();
+        return foundFriends;
     }
 
-    public CommonResponse addBlockUserList(String sessionId, String friendId) {
+    @Transactional
+    public ChangeFriendStateResponse changeFriendState(String sessionId, String friendId, ChangeFriendStateRequest request) {
         User sessionUser = sessionRepository.get(sessionId);
         if (Objects.nonNull(friendRepository.selectFriend(sessionUser, friendId))) {
-            return CommonResponse.failWith("Not Exists in friend list");
+            throw new FriendNotExistsException();
         }
 
-        friendRepository.updateFriendBlocked(sessionUser, friendId);
-        return CommonResponse.success();
+        friendRepository.updateFriendState(sessionUser, friendId, request);
+        return ChangeFriendStateResponse.success();
     }
 
+    public boolean isExistInBlockList(String sessionId, String friendId) {
+        User sessionUser = sessionRepository.get(sessionId);
+
+        return friendRepository.existFriendInBlockedList(sessionUser, friendId);
+    }
 
 }
--- src/main/java/com/dlrtn/websocket/chat/business/user/application/UserService.java
+++ src/main/java/com/dlrtn/websocket/chat/business/user/application/UserService.java
@@ -1,10 +1,12 @@
 package com.dlrtn.websocket.chat.business.user.application;
 
+import com.dlrtn.websocket.chat.business.user.exception.AlreadyExistEmailException;
+import com.dlrtn.websocket.chat.business.user.exception.UserInfoNotMatchedException;
 import com.dlrtn.websocket.chat.business.user.model.domain.User;
 import com.dlrtn.websocket.chat.business.user.model.payload.*;
 import com.dlrtn.websocket.chat.business.user.repository.InMemorySessionRepository;
 import com.dlrtn.websocket.chat.business.user.repository.UserRepository;
-import com.dlrtn.websocket.chat.common.model.ResponseMessage;
+import com.dlrtn.websocket.chat.common.exception.CommonException;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -29,7 +31,7 @@ public class UserService {
         User foundUser = userRepository.findByUsername(request.getUsername());
 
         if (Objects.nonNull(foundUser)) {
-            return SignUpResponse.failWith(ResponseMessage.EXISTED_USER_ID);
+            throw new AlreadyExistEmailException();
         }
 
         LocalDateTime now = LocalDateTime.now();
@@ -47,7 +49,7 @@ public class UserService {
             userRepository.save(user);
             return SignUpResponse.success();
         } catch (Exception e) {
-            return SignUpResponse.failWith(ResponseMessage.SERVER_ERROR);
+            throw new CommonException(e);
         }
     }
 
@@ -59,7 +61,7 @@ public class UserService {
         User foundUser = userRepository.findByUsername(request.getUsername());
 
         if (hasNotMatchedPassword(foundUser, request.getPassword())) {
-            return SignInResponse.failWith("User id or password mismatch");
+            throw new UserInfoNotMatchedException();
         }
 
         String newSessionId = UUID.randomUUID().toString();
@@ -80,7 +82,7 @@ public class UserService {
         User sessionUser = getSessionUser(sessionId);
 
         if (hasNotMatchedPassword(sessionUser, request.getExistingPassword())) {
-            return ChangeUserProfileResponse.failWith("password not correct");
+            return ChangeUserProfileResponse.success();
         }
 
         String newRealName = StringUtils.defaultIfEmpty(request.getNewRealName(), sessionUser.getRealName());
@@ -109,7 +111,7 @@ public class UserService {
         User sessionUser = getSessionUser(sessionId);
 
         if (hasNotMatchedPassword(sessionUser, request.getPassword())) {
-            return WithdrawUserResponse.failWith("password not correct");
+            throw new UserInfoNotMatchedException();
         }
 
         userRepository.delete(sessionUser.getUsername());
