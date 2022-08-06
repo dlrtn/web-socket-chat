80e05ae76063185329c261fea0553183f1a71894
dlrtn <87226112+dlrtn@users.noreply.github.com>
2022-08-06 22:03:15 +0900

    DB 변경사항 반영

--- src/main/java/com/dlrtn/websocket/chat/business/chat/repository/ChatMemberRepository.java
+++ src/main/java/com/dlrtn/websocket/chat/business/chat/repository/ChatMemberRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static com.dlrtn.websocket.chat.business.chat.model.domain.generated.study_db.tables.TbChatroomMember.TB_CHATROOM_MEMBER;
+import static com.dlrtn.websocket.chat.business.chat.model.domain.generated.study_db.Tables.TB_CHAT_MEMBER;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,29 +18,29 @@ public class ChatMemberRepository {
 
     public List<ChatMember> selectChatMembers(String chatId) {
         return dslContext.select()
-                .from(TB_CHATROOM_MEMBER)
-                .where(TB_CHATROOM_MEMBER.CHATID.eq(chatId))
+                .from(TB_CHAT_MEMBER)
+                .where(TB_CHAT_MEMBER.CHATID.eq(chatId))
                 .fetchInto(ChatMember.class);
     }
 
     public void updateChatMemberRole(String userId, ChatMemberRole role) {
-        dslContext.update(TB_CHATROOM_MEMBER)
-                .set(TB_CHATROOM_MEMBER.ROLE, role.name())
-                .where(TB_CHATROOM_MEMBER.USERID.eq(userId))
+        dslContext.update(TB_CHAT_MEMBER)
+                .set(TB_CHAT_MEMBER.ROLE, role.name())
+                .where(TB_CHAT_MEMBER.USERID.eq(userId))
                 .execute();
     }
 
     public ChatMember selectChatRoomMemberById(String userId, String chatId) {
         return dslContext.select()
-                .from(TB_CHATROOM_MEMBER)
-                .where(TB_CHATROOM_MEMBER.USERID.eq(userId).and(TB_CHATROOM_MEMBER.CHATID.eq(chatId)))
+                .from(TB_CHAT_MEMBER)
+                .where(TB_CHAT_MEMBER.USERID.eq(userId).and(TB_CHAT_MEMBER.CHATID.eq(chatId)))
                 .fetchOneInto(ChatMember.class);
     }
 
     public boolean existsChatMember(String userId, String chatId) {
         return dslContext.select()
-                .from(TB_CHATROOM_MEMBER)
-                .where(TB_CHATROOM_MEMBER.CHATID.eq(chatId).and(TB_CHATROOM_MEMBER.USERID.eq(userId)))
+                .from(TB_CHAT_MEMBER)
+                .where(TB_CHAT_MEMBER.CHATID.eq(chatId).and(TB_CHAT_MEMBER.USERID.eq(userId)))
                 .execute() == 1;
     }
 
